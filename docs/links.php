19df29befe92d805f419c02503749031ea396d82
yusuke1998 <jhonnyjose1998@gmail.com>
2019-10-28 16:07:13 -0400

    agregando documentacion guia y img de practica

---
+++ docs/links.php
@@ -0,0 +1,4 @@
+muy importanteeee!
+
+https://github.com/Favi0/Favi0.github.io/wiki/Week1-Detecci%C3%B3n
+http://grupo.us.es/gtocoma/pid/pid2/pid24.htm
---
+++ src/Filters.py
@@ -0,0 +1,9 @@
+import scipy.ndimage.morphology as morp
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+
+class Filters:
+
+	def GrayScale:
+		
